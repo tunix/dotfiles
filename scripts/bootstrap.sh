92726aad2f1a26d5e0c5aaffddecb16267e4bcb9
Alper Kanat <alperkanat@raptiye.org>
2013-12-31 17:01:54 +0200

    platform changes added

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,6 +1,31 @@
 #!/bin/zsh
 
-DOTFILES_ROOT=$(pwd)
+DOTFILES_ROOT="$(pwd)"
+PLATFORM="$(uname)"
+SERVER_APPS_PATH="/srv/apps"
+SERVER_VENV_PATH="/srv/virtualenvs"
+OSX_TMUX_CHANGES=<<EOF
+
+# clipboard
+set-option -g default-command "reattach-to-user-namespace -l zsh" # or bash...
+bind C-c run "tmux save-buffer - | reattach-to-user-namespace pbcopy"
+bind C-v run "reattach-to-user-namespace pbpaste | tmux load-buffer - && tmux paste-buffer"
+EOF
+
+# deleting existing files first
+rm -rf $HOME/.bin/arch-packages-news.py
+rm -rf $HOME/.bin/genpwd
+rm -rf $HOME/.zshrc
+rm -rf $HOME/.ssh/authorized_keys
+rm -rf $HOME/.gitconfig
+rm -rf $HOME/.oh-my-zsh
+rm -rf $HOME/.vim*
+rm -rf $HOME/.isort.cfg
+rm -rf $HOME/.pythonrc
+rm -rf $HOME/.tmux.conf
+
+# resetting changes to these files
+git checkout .
 
 # initializing submodules
 git submodule init
@@ -32,11 +57,16 @@ ln -sf $DOTFILES_ROOT/settings/isort.cfg $HOME/.isort.cfg
 ln -sf $DOTFILES_ROOT/settings/pythonrc $HOME/.pythonrc
 ln -sf $DOTFILES_ROOT/settings/tmux.conf $HOME/.tmux.conf
 
-# environment related changes
-# TODO: server .zshrc changes
-# TODO: tmux.conf linux changes
+# server path definition changes
+if [[ "$(hostname -s)" != pengu* ]]; then
+    sed -i -e 's|$HOME/projects|/srv/apps|g' settings/zshrc
+    sed -i -e 's|$HOME/.virtualenvs|/srv/virtualenvs|g' settings/zshrc
+fi
+
+# OSX tmux changes
+if [[ $PLATFORM == "Darwin" ]]; then
+    echo $OSX_TMUX_CHANGES >> $HOME/.tmux.conf
+fi
 
 # enabling settings
 source $HOME/.zshrc &> /dev/null
-
-echo "Environment successfully created!"
