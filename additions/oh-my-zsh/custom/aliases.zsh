45c5c205723193a4de4ca3600ba3e768551b9f29
Alper Kanat <alper.kanat@commencis.com>
2020-06-22 17:14:39 +0300

    added alias for pruning git tags

--- additions/oh-my-zsh/custom/aliases.zsh
+++ additions/oh-my-zsh/custom/aliases.zsh
@@ -3,3 +3,4 @@ alias vim="nvim"
 alias ls="ls --group-directories-first --color"
 alias ll="ls -l --group-directories-first --color"
 alias open="xdg-open"
+alias gpt="git tag -l | xargs git tag -d && git fetch -t" # prune local tags & fetch remote tags
