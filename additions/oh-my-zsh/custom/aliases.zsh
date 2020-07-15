8f1b71eb9de0eb2b3b0e95f7b1724f70a9b55f20
Alper Kanat <alper.kanat@commencis.com>
2020-07-15 12:34:36 +0300

    Added alias for forcing C locale with gradle

--- additions/oh-my-zsh/custom/aliases.zsh
+++ additions/oh-my-zsh/custom/aliases.zsh
@@ -4,3 +4,4 @@ alias ls="ls --group-directories-first --color"
 alias ll="ls -l --group-directories-first --color"
 alias open="xdg-open"
 alias gpt="git tag -l | xargs git tag -d && git fetch -t" # prune local tags & fetch remote tags
+alias gradle="LC_ALL=C gradle"
