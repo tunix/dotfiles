319bc20f3eebb5ae3847cb886e3781826028ce6c
Alper Kanat <alper.kanat@commencis.com>
2020-07-22 11:53:47 +0300

    Added new alias for df to ignore squashfs mounts

--- additions/oh-my-zsh/custom/aliases.zsh
+++ additions/oh-my-zsh/custom/aliases.zsh
@@ -5,3 +5,4 @@ alias ll="ls -l --group-directories-first --color"
 alias open="xdg-open"
 alias gpt="git tag -l | xargs git tag -d && git fetch -t" # prune local tags & fetch remote tags
 alias gradle="LC_ALL=C gradle"
+alias df="df -x squashfs"
