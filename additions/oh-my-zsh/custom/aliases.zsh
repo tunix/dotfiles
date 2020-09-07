51daf8b06f345106a571596cb416c4eabe286003
Alper Kanat <me@alperkan.at>
2020-09-07 11:12:27 +0300

    Moved scriptable aliases into functions, removed alias for gradle

--- additions/oh-my-zsh/custom/aliases.zsh
+++ additions/oh-my-zsh/custom/aliases.zsh
@@ -3,7 +3,13 @@ alias vim="nvim"
 alias ls="ls --group-directories-first --color"
 alias ll="ls -l --group-directories-first --color"
 alias open="xdg-open"
-alias gpt="git tag -l | xargs git tag -d && git fetch -t" # prune local tags & fetch remote tags
-alias gradle="LC_ALL=C gradle"
 alias df="df -x squashfs"
-alias bat="batcat"
+
+# prune local tags & fetch remote tags
+gpt() {
+    git tag -l | xargs git tag -d && git fetch -t
+}
+
+bat() {
+    batcat "$@"
+}
