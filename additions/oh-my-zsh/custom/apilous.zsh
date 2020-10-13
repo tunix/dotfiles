2a3bb8fdb9c702fd294cf6b90ecdcb4bad18cd4f
Alper Kanat <me@alperkan.at>
2020-10-14 00:25:44 +0300

    Minor quoting fixes

--- additions/oh-my-zsh/custom/apilous.zsh
+++ additions/oh-my-zsh/custom/apilous.zsh
@@ -1,5 +1,5 @@
 export APILOUS_HOME="$HOME/Google Drive/apilo.us"
 
-if [[ -f $APILOUS_HOME/init.sh ]]; then
-    . $APILOUS_HOME/init.sh
+if [[ -f "$APILOUS_HOME"/init.sh ]]; then
+    . "$APILOUS_HOME"/init.sh
 fi
--- additions/oh-my-zsh/custom/commencis.zsh
+++ additions/oh-my-zsh/custom/commencis.zsh
@@ -1,5 +1,5 @@
 export COMMENCIS_HOME="$HOME/Google Drive/Documents/corporate/commencis"
 
-if [[ -f $COMMENCIS_HOME/init.sh ]]; then
-    . $COMMENCIS_HOME/init.sh
+if [[ -f "$COMMENCIS_HOME"/init.sh ]]; then
+    . "$COMMENCIS_HOME"/init.sh
 fi
