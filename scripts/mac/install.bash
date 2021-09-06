409b65d5cc9eaca5c21afb80efdd554e024dcf8e
Alper Kanat <me@alperkan.at>
2021-09-06 11:19:08 +0300

    Package installation is now optional for Mac

--- scripts/mac/install.bash
+++ scripts/mac/install.bash
@@ -1,23 +1,27 @@
 #!/bin/bash
 # vim:set ft=bash et sw=4:
 
-BREW_FLAGS="-q"
+if ask "Continue package installation?"; then
+    BREW_FLAGS="-q"
 
-CORE_PACKAGES=(
-    fish
-    neovim
-    the_silver_searcher
-    tmux
-    wget
-)
+    CORE_PACKAGES=(
+        fish
+        neovim
+        the_silver_searcher
+        tmux
+        wget
+    )
 
-echo "mac"
+    echo "mac"
 
-# TODO: modify PATH (via ~/.profile)
+    # TODO: modify PATH (via ~/.profile)
 
-# install brew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    # install brew
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install $BREW_FLAGS ${CORE_PACKAGES[@]}
+    brew install $BREW_FLAGS ${CORE_PACKAGES[@]}
 
-exit 0
+    exit 0
+else
+    echo "Continuing without installating packages..."
+fi
