d760af166b872aa9df77cfdde62feff0b5cf924a
Alper Kanat <me@alperkan.at>
2021-09-15 16:51:26 +0300

    Added Brewfile based Mac package installation

--- scripts/mac/install.bash
+++ scripts/mac/install.bash
@@ -4,24 +4,12 @@
 if ask "Continue package installation?"; then
     BREW_FLAGS="-q"
 
-    CORE_PACKAGES=(
-        fish
-        neovim
-        the_silver_searcher
-        tmux
-        wget
-    )
-
-    echo "mac"
-
     # TODO: modify PATH (via ~/.profile)
 
     # install brew
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-    brew install $BREW_FLAGS ${CORE_PACKAGES[@]}
-
-    exit 0
+    brew bundle $BREW_FLAGS --file=$DOTFILES_ROOT/scripts/mac/Brewfile
 else
     echo "Continuing without installating packages..."
 fi
