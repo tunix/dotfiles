2327b24ffc22ba9630ed50278b7d22666a3e10c4
Alper Kanat <me@alperkan.at>
2022-12-08 13:43:40 +0300

    Updated lvim & tmux configs

--- roles/configuration/files/lvim/config.lua
+++ roles/configuration/files/lvim/config.lua
@@ -1,6 +1,5 @@
-lvim.log.level = "warn"
+lvim.log.level = "info"
 lvim.format_on_save = true
-lvim.colorscheme = "onedarker"
 lvim.transparent_window = true
 
 lvim.leader = "space"
@@ -8,27 +7,39 @@ lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
-lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.view.width = 50
 
+lvim.builtin.treesitter.auto_install = true
 lvim.builtin.treesitter.ensure_installed = {
-  "bash",
-  "c",
-  "javascript",
-  "json",
-  "lua",
-  "python",
-  "typescript",
-  "tsx",
-  "css",
-  "rust",
-  "java",
-  "yaml",
+    "bash",
+    "javascript",
+    "json",
+    "lua",
+    "python",
+    "typescript",
+    "tsx",
+    "css",
+    "rust",
+    "java",
+    "yaml",
 }
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
 
+vim.opt.termguicolors = true
+vim.opt.hidden = true
+vim.opt.hlsearch = true
+vim.opt.smartcase = true
+vim.opt.smartindent = true
 vim.opt.guicursor = ""
+vim.opt.writebackup = false
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.relativenumber = true
+vim.opt.wrap = true
+vim.opt.textwidth = 120
+vim.opt.colorcolumn = 120
