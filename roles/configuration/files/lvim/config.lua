cfa2765d03bf14a7d76ce55c141726bd45005c33
Alper Kanat <me@alperkan.at>
2022-12-12 13:57:01 +0300

    Enabled S-l & S-h key bindings for lvim

--- roles/configuration/files/lvim/config.lua
+++ roles/configuration/files/lvim/config.lua
@@ -22,9 +22,8 @@ lvim.format_on_save = {
 lvim.leader = "space"
 -- add your own keymapping
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
-
--- lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
--- lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
+lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
+lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 
 -- -- Use which-key to add extra bindings with the leader-key prefix
 -- lvim.builtin.which_key.mappings["W"] = { "<cmd>noautocmd w<cr>", "Save without formatting" }
