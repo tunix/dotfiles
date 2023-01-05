1ef055fe710d8505579c9cd9a0699fec8d20cfe3
Alper Kanat <me@alperkan.at>
2023-01-06 01:11:08 +0300

    Fixed issues with static binary installation scripts

--- roles/configuration/files/static/install_kubectx.sh
+++ roles/configuration/files/static/install_kubectx.sh
@@ -6,6 +6,6 @@ BINARY_PATH="$HOME/.local/bin"
 DOWNLOAD_URL=$(curl -Ls https://api.github.com/repos/ahmetb/kubectx/releases/latest \
     | jq -r '.assets[] | select(.name | test("kubectx.*'${KERNEL,,}'_'$ARCHITECTURE'.*")) | .browser_download_url')
 
-curl -L $DOWNLOAD_URL -o $BINARY_PATH/kubectx
+curl -L $DOWNLOAD_URL | tar zxvf - -C $BINARY_PATH kubectx
 
 chmod +x $BINARY_PATH/kubectx
