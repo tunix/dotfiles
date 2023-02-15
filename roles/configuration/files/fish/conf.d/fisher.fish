bd95cf983f3240631c09937d27127b2de69510ff
Alper Kanat <me@alperkan.at>
2023-02-15 14:55:32 +0300

    Added installation & configuration for nvm & bass (for fish)

--- roles/configuration/files/fish/conf.d/fisher.fish
+++ roles/configuration/files/fish/conf.d/fisher.fish
@@ -1,18 +1,12 @@
 if status --is-login
-
     if not test -f $HOME/.config/fish/fish_plugins
-
-        curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish \
-            | source && fisher install jorgebucaran/fisher
+        curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source \
+            && fisher install jorgebucaran/fisher
 
         if test -f $HOME/.config/fish/fish_plugins_ansible
-
             cat $HOME/.config/fish/fish_plugins_ansible >> $HOME/.config/fish/fish_plugins
 
             fisher update
-
         end
-
     end
-
 end
