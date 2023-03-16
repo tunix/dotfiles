e9056a1d7a8c7bf0b562e853186892f963eb9e99
Alper Kanat <me@alperkan.at>
2023-03-17 01:40:13 +0300

    Fixed some common conf, kind script
    Signed-off-by: Alper Kanat <me@alperkan.at>

--- roles/installers/files/install_kind.sh
+++ roles/installers/files/install_kind.sh
@@ -6,6 +6,6 @@ BINARY_PATH="$HOME/.local/bin"
 DOWNLOAD_URL=$(curl -Ls https://api.github.com/repos/kubernetes-sigs/kind/releases/latest \
     | jq -r '.assets[] | select(.name | test("kind-'${KERNEL,,}'-'${ARCHITECTURE,,}'$")) | .browser_download_url')
 
-curl -L $DOWNLOAD_URL -o $BINARY_PATH kind
+curl -L $DOWNLOAD_URL -o $BINARY_PATH/kind
 
 chmod +x $BINARY_PATH/kind
