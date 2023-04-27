21eab376105f1e0e0fb04f3c79f203ec8ef22514
Alper Kanat <me@alperkan.at>
2023-04-27 08:45:43 +0300

    Updated some installers for Mac

--- roles/installers/files/install_cosign.sh
+++ roles/installers/files/install_cosign.sh
@@ -4,14 +4,8 @@ ARCHITECTURE=$(uname -m)
 KERNEL=$(uname -s)
 BINARY_PATH="$HOME/.local/bin"
 
-if [[ "$ARCHITECTURE" == "arm64" ]]; then
-    ARCHITECTURE="aarch64"
-else
-    ARCHITECTURE="amd64"
-fi
-
 DOWNLOAD_URL=$(curl -Ls https://api.github.com/repos/sigstore/cosign/releases/latest \
-    | jq -r '.assets[] | select(.name | test("cosign-'${KERNEL,,}'-'${ARCHITECTURE}'$")) | .browser_download_url')
+    | jq -r '.assets[] | select(.name | test("cosign-'${KERNEL,,}'-'${ARCHITECTURE/x86_64/amd64}'$")) | .browser_download_url')
 
 curl -L $DOWNLOAD_URL -o $BINARY_PATH/cosign
 
--- roles/installers/files/install_gh.sh
+++ roles/installers/files/install_gh.sh
@@ -3,9 +3,18 @@
 export ARCHITECTURE=$(uname -m)
 export KERNEL=$(uname -s)
 export BINARY_PATH="$HOME/.local/bin"
-export DOWNLOAD_URL=$(curl -Ls https://api.github.com/repos/cli/cli/releases/latest \
-    | jq -r '.assets[] | select(.name | test("gh_.*_'${KERNEL,,}'_'${ARCHITECTURE/x86_64/amd64}'.tar.gz")) | .browser_download_url')
 
-curl -L $DOWNLOAD_URL | tar zxvf - -C $BINARY_PATH --strip-components=2 '*/bin/gh'
+if [[ "$KERNEL" == "Darwin" ]]; then
+    export DOWNLOAD_URL=$(curl -Ls https://api.github.com/repos/cli/cli/releases/latest \
+        | jq -r '.assets[] | select(.name | test("gh_.*_macOS_'${ARCHITECTURE,,}'.zip")) | .browser_download_url')
+
+    curl -L $DOWNLOAD_URL -o /tmp/gh.zip
+    unzip -j /tmp/gh.zip 'gh_*/bin/gh' > $BINARY_PATH/gh
+else
+    export DOWNLOAD_URL=$(curl -Ls https://api.github.com/repos/cli/cli/releases/latest \
+        | jq -r '.assets[] | select(.name | test("gh_.*_'${KERNEL,,}'_'${ARCHITECTURE/x86_64/amd64}'.tar.gz")) | .browser_download_url')
+
+    curl -L $DOWNLOAD_URL | tar zxvf - -C $BINARY_PATH --strip-components=2 '*/bin/gh'
+fi
 
 chmod +x $BINARY_PATH/gh
