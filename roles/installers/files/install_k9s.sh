f6d7d7d774fa8215b02f99e8ec3dd5b25817173c
Alper Kanat <me@alperkan.at>
2023-05-07 19:26:52 +0300

    Fixes to installers of k9s & kind

--- roles/installers/files/install_k9s.sh
+++ roles/installers/files/install_k9s.sh
@@ -4,7 +4,7 @@ export ARCHITECTURE=$(uname -m)
 export KERNEL=$(uname -s)
 export BINARY_PATH="$HOME/.local/bin"
 export DOWNLOAD_URL=$(curl -Ls https://api.github.com/repos/derailed/k9s/releases/latest \
-    | jq -r '.assets[] | select(.name == "k9s_'$KERNEL'_'$ARCHITECTURE'.tar.gz") | .browser_download_url')
+    | jq -r '.assets[] | select(.name == "k9s_'$KERNEL'_'${ARCHITECTURE/x86_64/amd64}'.tar.gz") | .browser_download_url')
 
 curl -L $DOWNLOAD_URL | tar zxvf - -C $BINARY_PATH k9s
 
--- roles/installers/files/install_kind.sh
+++ roles/installers/files/install_kind.sh
@@ -4,7 +4,7 @@ ARCHITECTURE=$(uname -m)
 KERNEL=$(uname -s)
 BINARY_PATH="$HOME/.local/bin"
 DOWNLOAD_URL=$(curl -Ls https://api.github.com/repos/kubernetes-sigs/kind/releases/latest \
-    | jq -r '.assets[] | select(.name | test("kind-'${KERNEL,,}'-'${ARCHITECTURE,,}'$")) | .browser_download_url')
+    | jq -r '.assets[] | select(.name | test("kind-'${KERNEL,,}'-'${ARCHITECTURE/x86_64/amd64}'$")) | .browser_download_url')
 
 curl -L $DOWNLOAD_URL -o $BINARY_PATH/kind
 
