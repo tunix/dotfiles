0ba1f5c995ab173f109c40351c6003b9f8067f53
Alper Kanat <me@alperkan.at>
2023-05-29 17:25:22 +0300

    Reorganized configuration, added k9s settings

--- roles/configuration/files/fish/conf.d/init.fish
+++ roles/configuration/files/fish/conf.d/init.fish
@@ -4,4 +4,7 @@ if status --is-login
 
     # add ~/.local/bin to PATH
     fish_add_path ~/.local/bin {{ shell.path }}
+
+    # define XDG_CONFIG_HOME
+    set -Ux XDG_CONFIG_HOME $HOME/.config
 end
