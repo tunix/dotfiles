9be19f5c78add7504347d0a187c9bf79ac086f67
Alper Kanat <me@alperkan.at>
2024-05-30 15:58:41 +0300

    Removes installers in favor of brew

--- roles/installers/files/install_nvm.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-NVM_VERSION=v0.39.3
-
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/$NVM_VERSION/install.sh | bash
--- roles/installers/files/install_starship.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-curl -sS https://starship.rs/install.sh | sh -s -- -y -b ~/.local/bin/
